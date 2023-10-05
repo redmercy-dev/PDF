5a498d350f59b80c8de5cdff4f44a663c2e4c497
redmercy-dev <56046853+redmercy-dev@users.noreply.github.com>
2023-10-05 23:25:10 +0100

    Update Youtubelang.py

--- Youtubelang.py
+++ Youtubelang.py
@@ -1,7 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from dotenv import load_dotenv
 from pytesseract import image_to_string
 from PIL import Image
 from io import BytesIO
@@ -12,8 +11,6 @@ from tempfile import NamedTemporaryFile
 import pandas as pd
 import json
 import requests
-
-load_dotenv()
 
 
 # 1. Convert PDF file into images via pypdfium2
